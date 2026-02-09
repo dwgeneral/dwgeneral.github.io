00528502896293c036618aa731c7fa05669a4030
浪不能停 <guowind@live.com>
2026-02-09 20:56:20 +0800

    chore: 配置 GitHub Pages 部署

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   distDir: "dist",
+  basePath: "/keepsurfing",
+  assetPrefix: "/keepsurfing",
   images: {
     unoptimized: true,
   },
